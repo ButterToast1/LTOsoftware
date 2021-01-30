77315348e4ed54f186b3c4d656d79a2ea6007072
Jian Lasam <jian_lasam@dlsu.edu.ph>
2021-01-30 21:04:14 +0800

    This is the newest version including new scene

--- src/ph/edu/dlsu/lbycpa2/Controller.java
+++ src/ph/edu/dlsu/lbycpa2/Controller.java
@@ -1,4 +1,26 @@
 package ph.edu.dlsu.lbycpa2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+
+
+    @FXML
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+    }
+
 }
