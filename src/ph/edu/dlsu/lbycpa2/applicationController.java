ba100ee0603ec01831db5b942ae8912beec90f52
Pyro Belicena <john_pyro_belicena@dlsu.edu.ph>
2021-01-31 20:54:12 +0800

    revised application

--- src/ph/edu/dlsu/lbycpa2/applicationController.java
+++ src/ph/edu/dlsu/lbycpa2/applicationController.java
@@ -1,4 +1,26 @@
 package ph.edu.dlsu.lbycpa2;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class applicationController {
+    @FXML
+    TextField nameTextField, addressTextField,telTextField,tinTextField,nationalityTextField;
+    @FXML
+    Button clear, submit;
+    String name,address,tel,tin,nationality;
+    @FXML
+    public void clearhandleButtonClick(ActionEvent event)  {
+        
+    }
+    @FXML
+    public void submithandleButtonClick(ActionEvent event){
+
+    }
+
 }
