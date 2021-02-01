44de7c5277b73fcd3b2f707ef5ed2a5dac71e100
Jian Lasam <jian_lasam@dlsu.edu.ph>
2021-02-01 17:22:50 +0800

    removed exit button in MainMenu.fxml

--- src/ph/edu/dlsu/lbycpa2/mainMenuController.java
+++ src/ph/edu/dlsu/lbycpa2/mainMenuController.java
@@ -1,30 +1,17 @@
 package ph.edu.dlsu.lbycpa2;
-
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.io.IOException;
+
 
 public class mainMenuController {
 
-    @FXML
-    public void changeScreenButtonPushed(ActionEvent event) throws IOException {
-        Parent tableViewParent = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene tableViewScene = new Scene(tableViewParent);
 
-        //This line gets the Stage information
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        window.setScene(tableViewScene);
-        window.show();
-    }
-
+    
     public void changeScreenButtontoProfile(ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("profile.fxml"));
         Scene profileScene = new Scene(tableViewParent);
