3a4850313ed88cfaeda4447c8e49b71e9827acac
pyrobelicena1 <john_pyro_belicena@dlsu.edu.ph>
2021-02-06 01:06:21 +0800

    please work

---
+++ src/ph/edu/dlsu/lbycpa2/payment2.java
@@ -0,0 +1,67 @@
+package ph.edu.dlsu.lbycpa2;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.beans.EventHandler;
+import java.awt.*;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+import javafx.event.*;
+import java.util.Stack;
+import javax.swing.*;
+import javafx.animation.TranslateTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import java.awt.*;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+
+import java.util.Stack;
+import javax.swing.*;
+
+public class payment2 {
+
+/*
+    public void cancel(EventHandler event) throws IOException{
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("payment.fxml"));
+        Scene mainMenuScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(paymentScene);
+        window.show();
+    }
+
+    public void pay(EventHandler event) throws IOException{
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        Scene mainMenuScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(mainMenuScene);
+        window.show();
+    }
+
+ */
+}
--- src/ph/edu/dlsu/lbycpa2/paymentController.java
+++ src/ph/edu/dlsu/lbycpa2/paymentController.java
@@ -20,7 +20,6 @@ import java.awt.*;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.util.Duration;
-
 import java.util.Stack;
 import javax.swing.*;
 
@@ -28,9 +27,11 @@ import javax.swing.*;
 public class paymentController{
 
         public Label total;
+        public int NewPrice;
         //public ListView list_items;
-        @FXML private ListView <String> list_items,cartList;
+        @FXML private ListView <String> list_items,cartList,list_items2;
         @FXML private TextField showPrice;
+        @FXML private Label new_total;
         ObservableList <String> paymentList,newcartlist;
 
         @FXML
@@ -39,11 +40,8 @@ public class paymentController{
                 paymentList = FXCollections.observableArrayList();
                 newcartlist = FXCollections.observableArrayList();
                 list_items.setItems(list);
-                //cartList.setItems(newcartlist);
-                //paymentList.add("Registration Fee");
-                //paymentList.add("License Application");
-                //paymentList.add("License Renewal");
-                //paymentList.add("Late Fee");
+
+
         }
 
         public void poptomainMenu(ActionEvent event) throws IOException {
@@ -58,7 +56,7 @@ public class paymentController{
         }
 
         public void addItems(ActionEvent event) {
-                int totalPrice = 1;
+                int totalPrice = 0;
                 String item = showPrice.getText();
                 list_items.getItems().addAll(item);
                 //int -> string -> label
@@ -90,6 +88,33 @@ public class paymentController{
         public void paymentSort2(ActionEvent event) throws IOException{
                 list.sort(String.CASE_INSENSITIVE_ORDER.reversed());
         }
+
+        public void addItemsToTab(ActionEvent event){
+                int index = list_items.getSelectionModel().getSelectedIndex();
+                int NewPrice = 0;
+                list_items2.getItems().addAll(list_items.getItems().remove(index));
+
+                //priceList to priceList2
+                //NewPrice = NewPrice + priceList.priceList2();
+                //put price accumulation here
+                new_total.setText(String.valueOf(NewPrice));
+        }
+
+        public void removeItemsFromTab(ActionEvent event){
+                int index = list_items2.getSelectionModel().getSelectedIndex();
+                if (index >= 0) {
+                        list_items.getItems().addAll(list_items2.getItems().remove(index));
+                }
+        }
+
+        public void paymentSort_alt(ActionEvent event) throws IOException{
+                list2.sort(String.CASE_INSENSITIVE_ORDER);
+        }
+
+        public void paymentSort2_alt(ActionEvent event) throws IOException{
+                list2.sort(String.CASE_INSENSITIVE_ORDER.reversed());
+        }
+
         @FXML
         ObservableList list = FXCollections.observableArrayList("License Renewal Fee - 985","Driving w/o License - 3000",
                 "No Seatbelt - 1000",
@@ -101,6 +126,9 @@ public class paymentController{
                 "Missing Car Parts - 5000" ,
                 "Illegal Plate - 5000",
                 "Smoke Belching - 6000");
+        ObservableList list2 = FXCollections.observableArrayList();
+        ObservableList priceList = FXCollections.observableArrayList(3000, 1000,5000,1000,10000,5000,50000,5000,5000,6000);
+        ObservableList priceList2 = FXCollections.observableArrayList();
         //"Registration Fee", "License Application","Late Fee","Illegal Parking","Beating the Red Light"
         /*,
 
